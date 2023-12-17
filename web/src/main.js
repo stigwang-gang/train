67a412fa7d8e1cf9e483e82b8d74b976e92de507
stigwang <wangg4664@gmail.com>
2023-12-17 11:14:22 -0500

    Add axios interceptor to print request parameters and return results

--- web/src/main.js
+++ web/src/main.js
@@ -4,5 +4,19 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css';
-
+import axios from "axios";
 createApp(App).use(store).use(router).use(Antd).mount('#app')
+/**
+* axios拦截器
+*/
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
