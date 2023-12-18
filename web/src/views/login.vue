2673808026557c745f1696524c6f5ec042fb3ea0
stigwang <wangg4664@gmail.com>
2023-12-18 12:36:02 -0500

    Vuex cooperates with h5 session cache to solve the problem of data loss when the browser refreshes

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -44,6 +44,7 @@ import { defineComponent, reactive } from 'vue';
 import axios from 'axios';
 import {notification} from "ant-design-vue";
 import { useRouter } from 'vue-router'
+import store from "@/store";
 
 export default defineComponent({
       name: "login-view",
@@ -76,6 +77,7 @@ export default defineComponent({
             notification.success({ description: '登录成功！' });
             // 登录成功，跳到控台主页
             router.push("/");
+            store.commit("setMember",data.content);
           } else {
             notification.error({ description: data.message });
           }
