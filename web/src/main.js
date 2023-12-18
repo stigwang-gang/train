bb7842978415c15e1f709ef9ebcb6bbd1e3dfb64
stigwang <wangg4664@gmail.com>
2023-12-18 14:45:30 -0500

    Add login-related interception for axios: bring token when requesting, and verify whether the return code is 401 when returning

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue'
+import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import axios from "axios";
 createApp(App).use(store).use(router).use(Antd).mount('#app')
@@ -11,6 +11,11 @@ createApp(App).use(store).use(router).use(Antd).mount('#app')
 */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
+    const _token = store.state.member.token;
+    if (_token) {
+        config.headers.token = _token;
+        console.log("请求headers增加token:", _token);
+    }
     return config;}, error => {
     return Promise.reject(error);
 });
@@ -18,6 +23,15 @@ axios.interceptors.response.use(function (response) {
     console.log('返回结果：', response);
     return response;}, error => {
     console.log('返回错误：', error);
+    const response = error.response;
+    const status = response.status;
+    if (status === 401) {
+        // 判断状态码是401 跳转到登录页
+        console.log("未登录或登录超时，跳到登录页");
+        store.commit("setMember", {});
+        notification.error({ description: "未登录或登录超时" });
+        router.push('/login');
+    }
     return Promise.reject(error);
 });
 
