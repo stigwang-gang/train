372a4a9821fb97707cd4d53d6d4f7fa1e6c1ea28
stigwang <wangg4664@gmail.com>
2023-12-19 15:35:55 -0500

    Add passenger table, generate persistence layer code, and add passenger type enumeration

---
+++ member/src/main/java/train/member/mapper/PassengerMapper.java
@@ -0,0 +1,30 @@
+package train.member.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import train.member.domain.Passenger;
+import train.member.domain.PassengerExample;
+
+public interface PassengerMapper {
+    long countByExample(PassengerExample example);
+
+    int deleteByExample(PassengerExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Passenger record);
+
+    int insertSelective(Passenger record);
+
+    List<Passenger> selectByExample(PassengerExample example);
+
+    Passenger selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Passenger record, @Param("example") PassengerExample example);
+
+    int updateByExample(@Param("record") Passenger record, @Param("example") PassengerExample example);
+
+    int updateByPrimaryKeySelective(Passenger record);
+
+    int updateByPrimaryKey(Passenger record);
+}
