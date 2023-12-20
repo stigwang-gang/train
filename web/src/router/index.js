fae71a1252186b8b2a0303ae17f73ce673bf6e46
stigwang <wangg4664@gmail.com>
2023-12-19 22:31:03 -0500

    Create a welcome page: add passenger management page, modify menu, realize page switching, and realize menu synchronization activation

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -5,15 +5,25 @@ const routes = [
   {
     path: '/login',
     component: () => import('../views/login.vue')
-  },
-  {
+  }, {
     path: '/',
     component: () => import('../views/main.vue'),
     meta: {
       loginRequire: true
     },
-  }
-]
+    children: [{
+      path: 'welcome',
+      component: () => import('../views/main/welcome.vue'),
+    }, {
+      path: 'passenger',
+      component: () => import('../views/main/passenger.vue'),
+    }]
+  },
+  {
+    path: '',
+    redirect: '/welcome'
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
