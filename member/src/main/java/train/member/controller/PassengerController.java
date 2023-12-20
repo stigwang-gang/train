1f9a60217203b7bec413c15308cc1643ee7373ee
stigwang <wangg4664@gmail.com>
2023-12-20 11:14:08 -0500

    Added a passenger list query interface, which can only view the passengers added by all current passengers.

--- member/src/main/java/train/member/controller/PassengerController.java
+++ member/src/main/java/train/member/controller/PassengerController.java
@@ -2,13 +2,15 @@ package train.member.controller;
 
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import train.context.LoginMemberContext;
+import train.member.req.PassengerQueryReq;
+import train.member.resp.PassengerQueryResp;
 import train.member.resp.PassengerSaveReq;
 import train.member.service.PassengerService;
 import train.resp.CommonResp;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -20,5 +22,11 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
         passengerService.save(req);
         return new CommonResp<>();
+    }
+    @GetMapping("/query-list")
+    public CommonResp<Object> queryList(@Valid PassengerQueryReq req) {
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
     }
 }
--- member/src/main/java/train/member/service/PassengerService.java
+++ member/src/main/java/train/member/service/PassengerService.java
@@ -2,13 +2,20 @@ package train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import train.context.LoginMemberContext;
 import train.member.domain.Passenger;
+import train.member.domain.PassengerExample;
 import train.member.mapper.PassengerMapper;
+import train.member.req.PassengerQueryReq;
+import train.member.resp.PassengerQueryResp;
 import train.member.resp.PassengerSaveReq;
 import train.util.SnowUtil;
+
+import java.util.List;
+
 @Service
 public class PassengerService {
 @Resource
@@ -22,5 +29,15 @@ public void save(PassengerSaveReq req) {
     passenger.setCreateTime(now);
     passenger.setUpdateTime(now);
     passengerMapper.insert(passenger);
+}
+
+public List<PassengerQueryResp> queryList(PassengerQueryReq req) {
+    PassengerExample passengerExample = new PassengerExample();
+    PassengerExample.Criteria criteria = passengerExample.createCriteria();
+    if (ObjectUtil.isNotNull(req.getMemberId())) {
+        criteria.andMemberIdEqualTo(req.getMemberId());
+    }
+    List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+    return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
 }
 }
