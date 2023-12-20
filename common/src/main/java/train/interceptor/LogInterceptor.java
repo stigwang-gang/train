27184e562305f0b05cbbdac4d00bae544d398b83
stigwang <wangg4664@gmail.com>
2023-12-19 21:15:42 -0500

    Use thread local variables to store member information, add log interceptors, and uniformly print log serial numbers

---
+++ common/src/main/java/train/interceptor/LogInterceptor.java
@@ -0,0 +1,17 @@
+package train.interceptor;
+
+import cn.hutool.core.util.RandomUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+               // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        return true;
+    }
+}
--- member/src/main/java/train/member/service/PassengerService.java
+++ member/src/main/java/train/member/service/PassengerService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import train.context.LoginMemberContext;
 import train.member.domain.Passenger;
 import train.member.mapper.PassengerMapper;
 import train.member.resp.PassengerSaveReq;
@@ -16,6 +17,7 @@ private PassengerMapper passengerMapper;
 public void save(PassengerSaveReq req) {
     DateTime now = DateTime.now();
     Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+    passenger.setMemberId(LoginMemberContext.getId());
     passenger.setId(SnowUtil.getSnowflakeNextId());
     passenger.setCreateTime(now);
     passenger.setUpdateTime(now);
