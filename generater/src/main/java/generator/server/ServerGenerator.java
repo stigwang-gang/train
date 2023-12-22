99a4a3fb0a239da8eceaab489c2dbe358fd73d8d
stigwang <wangg4664@gmail.com>
2023-12-22 17:42:37 -0500

    Add DbUtil to obtain table field information and generate saveReq entity class

--- generater/src/main/java/generator/server/ServerGenerator.java
+++ generater/src/main/java/generator/server/ServerGenerator.java
@@ -1,6 +1,8 @@
 package generator.server;
 
 import freemarker.template.TemplateException;
+import generator.util.DbUtil;
+import generator.util.Field;
 import generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -9,8 +11,7 @@ import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ServerGenerator {
     static String serverPath = "[module]/src/main/java/train/[module]/";
@@ -37,6 +38,18 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
+
         // 示例：表名 jiawa_test
         // Domain = JiawaTest
         String Domain = domainObjectName.getText();
@@ -44,14 +57,24 @@ public class ServerGenerator {
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
         // do_main = jiawa-test
         String do_main = tableName.getText().replaceAll("_", "-");
+
+        // 表中文名
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+        Set<String> typeSet = getJavaTypes(fieldList);
+
         // 组装参数
         Map<String, Object> param = new HashMap<>();
         param.put("Domain", Domain);
         param.put("domain", domain);
         param.put("do_main", do_main);
+        param.put("tableNameCn", tableNameCn);
+        param.put("fieldList", fieldList);
+        param.put("typeSet", typeSet);
         System.out.println("组装参数：" + param);
-        gen(Domain, param, "service");
-        gen(Domain, param, "controller");
+        gen(Domain, param, "service", "service");
+        gen(Domain, param, "controller", "controller");
+        gen(Domain, param, "req", "saveReq");
 
 
 
@@ -62,9 +85,9 @@ public class ServerGenerator {
 //        FreemarkerUtil.generator(toPath + "Test1.java", param);
     }
 
-    private static void gen(String Domain, Map<String, Object> param, String target) throws IOException, TemplateException {
+    private static void gen(String Domain, Map<String, Object> param, String packageName, String target) throws IOException, TemplateException {
         FreemarkerUtil.initConfig(target + ".ftl");
-        String toPath = serverPath + target + "/";
+        String toPath = serverPath + packageName + "/";
         new File(toPath).mkdirs();
         String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
         String fileName = toPath + Domain + Target + ".java";
@@ -81,5 +104,17 @@ public class ServerGenerator {
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
         return node.getText();
+    }
+
+    /**
+     * 获取所有的Java类型，使用Set去重
+     */
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
     }
 }
--- member/src/main/java/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/train/member/req/PassengerSaveReq.java
@@ -1,24 +1,52 @@
 package train.member.req;
 
+import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.util.Date;
-
 public class PassengerSaveReq {
+
+    /**
+     * id
+     */
     private Long id;
 
+    /**
+     * 会员id
+     */
+
     private Long memberId;
-    @NotBlank(message = "【名字】不能为空")
+
+    /**
+     * 姓名
+     */
+    @NotBlank(message = "【姓名】不能为空")
     private String name;
+
+    /**
+     * 身份证
+     */
     @NotBlank(message = "【身份证】不能为空")
     private String idCard;
+
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
     @NotBlank(message = "【旅客类型】不能为空")
     private String type;
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
@@ -79,14 +107,18 @@ public class PassengerSaveReq {
 
     @Override
     public String toString() {
-        return "PassengerSaveReq{" +
-                "id=" + id +
-                ", memberId=" + memberId +
-                ", name='" + name + '\'' +
-                ", idCard='" + idCard + '\'' +
-                ", type='" + type + '\'' +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", memberId=").append(memberId);
+        sb.append(", name=").append(name);
+        sb.append(", idCard=").append(idCard);
+        sb.append(", type=").append(type);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
     }
 }
