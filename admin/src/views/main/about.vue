bd2729aa3996b795ca991dbdc73644e3b5fddaf5
stigwang <wangg4664@gmail.com>
2023-12-23 10:29:33 -0500

    Create admin vue

---
+++ admin/src/views/main/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>关于12306</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name:"about_view",
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+<style>
+</style>
