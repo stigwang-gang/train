a1f90fc1eef3227d3932cb212b30cbaa44b766f5
stigwang <wangg4664@gmail.com>
2023-12-25 13:02:15 -0500

    Integrate pinyin-pro to automatically generate pinyin and initials

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -37,7 +37,7 @@
         <a-input v-model:value="trainStation.name" />
       </a-form-item>
       <a-form-item label="站名拼音">
-        <a-input v-model:value="trainStation.namePinyin" />
+        <a-input v-model:value="trainStation.namePinyin" disabled />
       </a-form-item>
       <a-form-item label="进站时间">
         <a-time-picker v-model:value="trainStation.inTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
@@ -56,7 +56,8 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
+import {pinyin} from "pinyin-pro";
+import {defineComponent, ref, onMounted, watch} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
 
@@ -131,6 +132,14 @@ export default defineComponent({
       dataIndex: 'operation'
     }
     ];
+
+    watch(() => trainStation.value.name, ()=>{
+      if (Tool.isNotEmpty(trainStation.value.name)) {
+        trainStation.value.namePinyin = pinyin(trainStation.value.name, { toneType: 'none'}).replaceAll(" ", "");
+      } else {
+        trainStation.value.namePinyin = "";
+      }
+    }, {immediate: true});
 
     const onAdd = () => {
       trainStation.value = {};
