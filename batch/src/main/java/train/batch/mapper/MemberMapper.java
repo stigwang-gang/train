523a1a6f3e790c5a460cb3e1adca42cfb2bec54a
stigwang <wangg4664@gmail.com>
2023-12-27 10:26:28 -0500

    Add a batch database to RDS, configure a dedicated database, and configure a persistence layer

---
+++ batch/src/main/java/train/batch/mapper/MemberMapper.java
@@ -0,0 +1,30 @@
+package train.batch.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import train.batch.domain.Member;
+import train.batch.domain.MemberExample;
+
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
+}
