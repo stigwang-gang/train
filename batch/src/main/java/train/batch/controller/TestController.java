0475bcca6a3b0672b45ae5e11371bacd5773ac38
stigwang <wangg4664@gmail.com>
2023-12-29 16:06:40 -0500

    Integrate openfeign to realize calls between services. Add an application name to each application, which can be used with the configuration center

--- batch/src/main/java/train/batch/controller/TestController.java
+++ batch/src/main/java/train/batch/controller/TestController.java
@@ -1,12 +1,24 @@
 package train.batch.controller;
 
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import train.batch.feign.BusinessFeign;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello() {
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
         return "Hello World! Batch!";
     }
 }
